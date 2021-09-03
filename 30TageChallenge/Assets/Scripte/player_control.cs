1833c0425903a6e2c91f3f97b9e10ffb6e83ddf0
Eric <info@ericmaechler.com>
2021-09-03 22:17:12 +0200

    add boden

--- 30TageChallenge/Assets/Scripte/player_control.cs
+++ 30TageChallenge/Assets/Scripte/player_control.cs
@@ -8,22 +8,7 @@ public class player_control : MonoBehaviour
 public float movementSpeed = 10.0f;
 public float rotationSpeed = 200.0f;
 
-public Rigidbody rig;
 
-/* 
-
-float x = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
-float z = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-rig.velocity = new Vector3(x, rig.velocity.y, z);
-
-Vector3 vel = rig.velocity;
-vel.y = 0;
-
-    if(vel.y != 0 || vel.z != 0)
-    {
-    //transform.forward = rig.velocity;
-    transform.forward = vel;
-    }
 
 
     // Update is called once per frame
